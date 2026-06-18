631d124876a4950b17b67a4ebaa9cd1164addc0e
sayantan <123sayantandas@gmail.com>
2026-06-18 19:44:48 +0530

    gif added

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + sayantan</h1>
+      <h1>Vite + React + sayantan + project 3</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -24,7 +24,7 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-        <img src="612503328_17906200185335708_3025303023599173422_n.jpg" style={{ width: '50%', height: 'auto' }} alt="Description" />
+        <img src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExZThiMDZodjZ0M3JieG5zajlpZW5zdzdxdDBpZ3R1ZGxhbGxvM2x6OCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/tXL4FHPSnVJ0A/giphy.gif" style={{ width: '50%', height: 'auto' }} alt="funny gif" />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
